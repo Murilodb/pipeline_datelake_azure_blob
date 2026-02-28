69fa7816974b919b5004d7227192f91b9542bbd5
murilo.moreira <xxmuriloxx0077@gmail.com>
2026-02-28 16:55:47 -0300

    fix: retirando as cores dos logs

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import logging
-import colorlog
+from dotenv import load_dotenv
 from get_data import DataDownloader, CompactArchive
+from salve_Azureblob import ConnectAzureBlobStorage, UploadBlobFile
 
 
 def configure_logging():
-    handler = colorlog.StreamHandler()
-    
-    formatter = colorlog.ColoredFormatter(
-        "%(log_color)s%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-        log_colors={
-            "DEBUG": "cyan",
-            "INFO": "green",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "bold_red",
-        }
-    )
-
-    handler.setFormatter(formatter)
-
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    logger.addHandler(handler)
+
+    if not logger.handlers:
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+        )
+
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
 
 def main():
+    load_dotenv()  
     configure_logging()
+
     downloader = DataDownloader(save_path="./data")
     downloader.download_all()
 
     compact_archive = CompactArchive(path="./data", filename="boston_data.zip")
     compact_archive.create_zip()
 
+    #coneectando ao azure e fazendo upload do arquivo zip criado
+    connectAzure = ConnectAzureBlobStorage(account_url="https://dadosbostonmurilo.blob.core.windows.net")
+    blob_service_client = connectAzure.get_blob_service_client_account_key()
+    if blob_service_client is not None:
+        uploader = UploadBlobFile(blob_service_client=blob_service_client, 
+                                    container_name="servicosbostonzip",
+                                    blob_name="boston_data.zip",
+                                    file_path="data/boston_data.zip",
+                                    file_name="boston_data.zip")
+        uploader.upload()
+
+
+
 if __name__ == "__main__":
     main()
+
